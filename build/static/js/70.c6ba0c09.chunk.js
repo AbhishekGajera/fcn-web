38b8c30803ec752683eb5b218c81fe6698b72389
ApexaBarvaliya <apexabar69@gmail.com>
2022-10-07 00:20:31 +0530

    release build

--- build/static/js/70.c6ba0c09.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppurple-react-free"]=this["webpackJsonppurple-react-free"]||[]).push([[70],{636:function(e,t,r){"use strict";r.r(t);var n=r(0),u=r.n(n);t.default=function(){return u.a.createElement("div",null,"PoEquity")}}}]);
-//# sourceMappingURL=70.c6ba0c09.chunk.js.map
