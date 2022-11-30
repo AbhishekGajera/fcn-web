6836263c2995e245c01d30a51ad52b9f8881d401
ApexaBarvaliya <apexabar69@gmail.com>
2022-12-01 00:08:51 +0530

    fix:dashboard notification & delete lead

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f9fdad8f494a73603d7943f8829d98a4.js"
+  "/precache-manifest.8bf399f24a90c51807bc748b45b79ffe.js"
 );
 
 self.addEventListener('message', (event) => {
