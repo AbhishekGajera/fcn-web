46a610652b33b1fd8921778f8c5eb1578af7f746
ApexaBarvaliya <apexabar69@gmail.com>
2022-12-21 23:47:11 +0530

    release build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d9bd57499fac95b90e34d541742f978.js"
+  "/precache-manifest.6bfcdb01012464990064b93f621c665b.js"
 );
 
 self.addEventListener('message', (event) => {
